e54c0bf1f138c0cb78ef07cc7f53d19ccb3a6a82
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-01-27 17:34:05 +0000

    pushing latest changes

--- assets/logic.js
+++
@@ -1,5 +0,0 @@
-
-var queryURL = https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0850dcccd6ea018c7162ae4c78fc0731}
-
-console.log(queryURL);
-
---
+++ assets/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // console.log("hello")
+
+    const apiKey = "0850dcccd6ea018c7162ae4c78fc0731";
+
+    // var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + apiKey + "&units=imperial";
+
+    // var geoURL = "https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + apiKey + "&units=imperial";
+
+    var citySearch = $("#search-input").val();
+    var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=imperial";
+
+console.log(cityURL);
+    
+
+
+    // fetch(cityURL)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         // Process the data returned from the API
+    //         console.log(data);
+    //     })
+    //     .catch(error => {
+    //         // Handle any errors that occurred during the fetch request
+    //         console.error(error);
+    //     });
+
+
+
+
+});
