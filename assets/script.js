7059e3dc1d7417a6c827d606e8dbb69a5403b2c4
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-01-28 13:08:47 +0000

    code to display weather for the main block

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,10 @@ $(document).ready(function () {
         var citySearch = $("#search-input").val();
         //   var for the query url inc API key and city generated from the user input
         // var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=imperial";
- 
+
         //   var for the query url inc API key and city generated from the user input
         var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey + "&units=imperial";
+        // console.log(weatherURL);
 
         // Fetch the data from the API
         fetch(weatherURL)
@@ -18,11 +19,22 @@ $(document).ready(function () {
             .then(data => {
                 // Process the data returned from the API
                 console.log(data);
+                // create var for current date
+                var currentDate = dayjs().format("DD/MM/YYYY");
+                // display the data on the page
+                $("#city").text("City: " + data.name + ", " + currentDate);
+                $("#temp").text("Temperature: " + data.main.temp + "°C");
+                $("#wind").text("Wind Speed: " + data.wind.speed + "mph");
+                $("#humidity").text("Humidity: " + data.main.humidity + "%");
+
+
+
             })
             .catch(error => {
                 // Handle any errors that occurred during the fetch request
                 console.error(error);
             });
+
     });
 
 
@@ -30,6 +42,6 @@ $(document).ready(function () {
 
 
 
-    
+
 
 });
