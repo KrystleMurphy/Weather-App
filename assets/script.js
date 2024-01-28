710abdfdbd876b9c2f492465d411145f52b5621e
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-01-28 12:33:58 +0000

    updating fetch

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const apiKey = "0850dcccd6ea018c7162ae4c78fc0731";
-    
-    $("#search-button").on("click", function(event) {
-      event.preventDefault(); // Prevent the default form submission
-      
-    //   variable for the user input
-      var citySearch = $("#search-input").val();
-    //   var for the query url inc API key and city generated from the user input
-      var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=imperial";
 
-      fetch(cityURL)
-        .then(response => response.json())
-        .then(data => {
-          // Process the data returned from the API
-          console.log(data);
-        })
-        .catch(error => {
-          // Handle any errors that occurred during the fetch request
-          console.error(error);
-        });
+    $("#search-button").on("click", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        //   variable for the user input
+        var citySearch = $("#search-input").val();
+        //   var for the query url inc API key and city generated from the user input
+        var cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + citySearch + "&limit=5&appid=" + apiKey + "&units=imperial";
+
+        fetch(cityURL)
+            .then(response => response.json())
+            .then(data => {
+                // Process the data returned from the API
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle any errors that occurred during the fetch request
+                console.error(error);
+            });
     });
-  });
-  
+});
